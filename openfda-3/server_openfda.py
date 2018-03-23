dea291c0be1e6afb5009f3d77a9cc9d3372676a7
obijuan <juan@iearobotics.com>
2018-03-23 12:44:19 +0100

    openfda-3: initial commit

---
+++ openfda-3/server_openfda.py
@@ -0,0 +1,48 @@
+import http.server
+import socketserver
+
+# --- Puerto donde lanzar el servidor
+PORT = 8000
+
+
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # -- Envio de la respuesta al cliente
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+
+        mensaje = """
+        <!DOCTYPE>
+        <html>
+        <body>
+        """
+
+        mensaje += "<p>" + "Hola mundo!" + "</p>"
+        mensaje += "<ul>"
+        mensaje += "<li>Recurso solicitado: {}</li>".format(self.path)
+        mensaje += "</ul>"
+        mensaje += "</body>"
+        mensaje += "</html>"
+
+        self.wfile.write(bytes(mensaje, "utf8"))
+        print("Petición atendida!")
+        return
+
+
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Sirviendo en puerto: {}".format(PORT))
+
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("Servidor detenido")
+    httpd.server_close()
+
